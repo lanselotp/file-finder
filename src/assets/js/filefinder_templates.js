1e580fd382022322357d930896927646dcd8e76c
unborn_hrostozov <peturhristozovps@gmail.com>
2018-12-13 13:52:06 +0200

    Add js template files.

---
+++ src/assets/js/filefinder_templates.js
@@ -0,0 +1,12 @@
+function getFileTemplate(file) {
+	return '<div class="panel panel-default">\n' +
+		'<div class="panel-body">\n' +
+		'<p>Directory - ' + file.directory +
+		'<p>File name - ' + file.name  +
+		'</div>\n' +
+		'</div>';
+}
+
+function getNoResultTemplate() {
+	return '<h3>No files content matched your search criteria !</h3>';
+}
